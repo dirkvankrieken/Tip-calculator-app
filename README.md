c6f515edb6e9065c2d39c25576ad36f76781d5b4
Dirk <45898176+dvkcds@users.noreply.github.com>
2025-03-16 17:49:53 +0100

    left todo: hover state for input boxes

--- README.md
+++ README.md
@@ -14,9 +14,6 @@ This is a solution to the [Tip calculator app challenge on Frontend Mentor](http
   - [Continued development](#continued-development)
   - [Useful resources](#useful-resources)
 - [Author](#author)
-- [Acknowledgments](#acknowledgments)
-
-**Note: Delete this note and update the table of contents based on what sections you keep.**
 
 ## Overview
 
@@ -32,14 +29,6 @@ Users should be able to:
 
 ![](./screenshot.jpg)
 
-Add a screenshot of your solution. The easiest way to do this is to use Firefox to view your project, right-click the page and select "Take a Screenshot". You can choose either a full-height screenshot or a cropped one based on how long the page is. If it's very long, it might be best to crop it.
-
-Alternatively, you can use a tool like [FireShot](https://getfireshot.com/) to take the screenshot. FireShot has a free option, so you don't need to purchase it. 
-
-Then crop/optimize/edit your image however you like, add it to your project, and update the file path in the image above.
-
-**Note: Delete this note and the paragraphs above when you add your screenshot. If you prefer not to add a screenshot, feel free to remove this entire section.**
-
 ### Links
 
 - Solution URL: [Add solution URL here](https://your-solution-url.com)
@@ -54,35 +43,12 @@ Then crop/optimize/edit your image however you like, add it to your project, and
 - Flexbox
 - CSS Grid
 - Mobile-first workflow
-- [React](https://reactjs.org/) - JS library
-- [Next.js](https://nextjs.org/) - React framework
-- [Styled Components](https://styled-components.com/) - For styles
-
-**Note: These are just examples. Delete this note and replace the list above with your own choices**
 
 ### What I learned
 
-Use this section to recap over some of your major learnings while working through this project. Writing these out and providing code samples of areas you want to highlight is a great way to reinforce your own knowledge.
+- how to take a value from an input form, how to use it as a number by parsing it from a string to an integer, so that calculations can be done with it by using the parseInt() function.
 
-To see how you can add code snippets, see below:
-
-```html
-<h1>Some HTML code I'm proud of</h1>
-```
-```css
-.proud-of-this-css {
-  color: papayawhip;
-}
-```
-```js
-const proudOfThisFunc = () => {
-  console.log('🎉')
-}
-```
-
-If you want more help with writing markdown, we'd recommend checking out [The Markdown Guide](https://www.markdownguide.org/) to learn more.
-
-**Note: Delete this note and the content within this section and replace with your own learnings.**
+-
 
 ### Continued development
 
@@ -104,9 +70,3 @@ Use this section to outline areas that you want to continue focusing on in futur
 - Twitter - [@yourusername](https://www.twitter.com/yourusername)
 
 **Note: Delete this note and add/remove/edit lines above based on what links you'd like to share.**
-
-## Acknowledgments
-
-This is where you can give a hat tip to anyone who helped you out on this project. Perhaps you worked in a team or got some inspiration from someone else's solution. This is the perfect place to give them some credit.
-
-**Note: Delete this note and edit this section's content as necessary. If you completed this challenge by yourself, feel free to delete this section entirely.**
---
+++ script.js
@@ -0,0 +1,116 @@
+const priceInputBox = document.getElementById('price')
+const tipPercentageArea = document.querySelector('.tip-percentage-container')
+const numberOfPeopleInputBox = document.getElementById('number-of-people')
+const resetButton = document.getElementById('reset-button')
+const customInput = document.querySelector('.tip-custom-input')
+
+priceInputBox.addEventListener('keyup', () => {
+  if (numberOfPeopleInputBox.value > 0 && priceInputBox.value > 0) {
+    if (
+      document
+        .querySelector('.tip-active')
+        .classList.contains('tip-custom-input')
+    ) {
+      calculateAmmounts(parseInt(document.querySelector('.tip-active').value))
+    } else {
+      calculateAmmounts(
+        parseInt(document.querySelector('.tip-active').innerText)
+      )
+    }
+  } else if (priceInputBox.value === '0') {
+    document.getElementById('price-not-zero').classList.remove('hide')
+  } else {
+    calculateAmmounts(0)
+    document.getElementById('price-not-zero').classList.add('hide')
+  }
+})
+
+customInput.addEventListener('keyup', () => {
+  calculateAmmounts(customInput.value)
+})
+
+numberOfPeopleInputBox.addEventListener('keyup', () => {
+  if (numberOfPeopleInputBox.value > 0 && priceInputBox.value > 0) {
+    if (
+      document
+        .querySelector('.tip-active')
+        .classList.contains('tip-custom-input')
+    ) {
+      calculateAmmounts(parseInt(document.querySelector('.tip-active').value))
+    } else {
+      calculateAmmounts(
+        parseInt(document.querySelector('.tip-active').innerText)
+      )
+    }
+  } else if (numberOfPeopleInputBox.value === '0') {
+    document.getElementById('numberofpeople-not-zero').classList.remove('hide')
+  } else {
+    calculateAmmounts(0)
+    document.getElementById('numberofpeople-not-zero').classList.add('hide')
+  }
+})
+
+function reset() {
+  priceInputBox.value = ''
+  numberOfPeopleInputBox.value = ''
+  customInput.value = ''
+  calculateAmmounts(0)
+}
+
+resetButton.addEventListener('click', reset)
+
+tipPercentageArea.addEventListener('click', (e) => {
+  if (e.target.classList.contains('tip-percentage')) {
+    if (!e.target.classList.contains('tip-active')) {
+      if (
+        document
+          .querySelector('.tip-active')
+          .classList.contains('tip-custom-input')
+      ) {
+        document.querySelector('.tip-custom-input').style.display = 'none'
+        document.querySelector('.tip-custom-input').value = ''
+        document.querySelector('.tip-custom').style.display = 'block'
+      }
+      document.querySelector('.tip-active').classList.remove('tip-active')
+      e.target.classList.add('tip-active')
+      calculateAmmounts(parseInt(e.target.innerText))
+    }
+  } else if (e.target.classList.contains('tip-custom')) {
+    calculateAmmounts(0)
+    document.querySelector('.tip-active').classList.remove('tip-active')
+    document.querySelector('.tip-custom-input').classList.add('tip-active')
+    e.target.style.display = 'none'
+    customInput.style.display = 'inline-block'
+    customInput.focus()
+  }
+})
+
+function calculateAmmounts(percentage) {
+  let priceValue = priceInputBox.value
+  let numberOfPeopleValue = numberOfPeopleInputBox.value
+  if (
+    parseFloat(priceValue) > 0 &&
+    parseInt(numberOfPeopleValue) > 0 &&
+    percentage
+  ) {
+    const price = parseFloat(priceValue)
+    const numberOfPeople = parseInt(numberOfPeopleValue)
+    const tipPerPerson = (price * (percentage / 100)) / numberOfPeople
+    const totalPerPerson = price / numberOfPeople + tipPerPerson
+    document.getElementById('tip').innerHTML = tipPerPerson.toFixed(2)
+    document.getElementById('total').innerHTML = totalPerPerson.toFixed(2)
+  } else {
+    if (priceInputBox.value === '0') {
+      document.getElementById('price-not-zero').classList.remove('hide')
+    }
+    if (numberOfPeopleInputBox.value === '0') {
+      document
+        .getElementById('numberofpeople-not-zero')
+        .classList.remove('hide')
+    }
+    document.getElementById('tip').innerHTML = ''
+    document.getElementById('total').innerHTML = ''
+  }
+}
+
+reset()
